ebc5cfeceb0040a11cf9c2e40cdf1578c45d50c0
YeYunfeng <269581661@qq.com>
2019-03-12 17:32:09 +0800

    修改配置部署新浪云

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ const fs = require("fs");
 const multer = require("multer");
 
 //3:指定监听端口3000
-app.listen(3030);
+app.listen(5050);
 //4:指定静态目录 public
 // __dirname 当前程序所属目录绝对路径 
 //app.js vue_app_server
--- pool.js
+++ pool.js
@@ -1,10 +1,15 @@
 const mysql = require("mysql");
 var pool = mysql.createPool({
-     host:"127.0.0.1",
-     port:3306,
-     user:"root",
-     password:"",
-     database:"xx",
-     connectionLimit:15
+     // host:"127.0.0.1",
+     // port:3306,
+     // user:"root",
+     // password:"",
+     // database:"xx",
+     host: process.env.MYSQL_HOST,
+     port: process.env.MYSQL_PORT,
+     user: process.env.ACCESSKEY,
+     password: process.env.SECRETKEY,
+     database: 'app_' + process.env.APPNAME,
+     connectionLimit: 15
 });
 module.exports = pool;
